c3febfac94a97d5d0e47ab5fe35a13fef85d2dee
DancingChochol <131389592+DancingChochol@users.noreply.github.com>
2023-06-19 21:14:03 +0200

    Wszystko gotowe w wyswietlaniu obserwujacyh

--- InstagramPlusProjekt-merge/InstPlusEntityFr/Pages/WyswObserwujacych/Index.cshtml.cs
+++ InstagramPlusProjekt-merge/InstPlusEntityFr/Pages/WyswObserwujacych/Index.cshtml.cs
@@ -6,26 +6,51 @@ namespace InstPlusEntityFr.Pages.WyswObserwujacych
     public class IndexModel : PageModel
     {
         public List<Uzytkownik> listaUzytkownikow { get; set; } = new List<Uzytkownik>();
-
+        public int czyKogosProfil = 1;
+        public int? id;
         public void OnGet()
         {
             DbInstagramPlus db = new DbInstagramPlus();
             int? zalogowanyId = (int?)HttpContext.Session.GetInt32("UzytkownikId");
+            int? kogosId = (int?)HttpContext.Session.GetInt32("SzukaneID");
 
+            if (kogosId != null)
+            {
+				if (zalogowanyId != null) czyKogosProfil = 2;
+                else czyKogosProfil = 0;
+                id = kogosId;
+                HttpContext.Session.Remove("SzukaneID");
+            }
+            
+            IQueryable<Obserwujacy> obserwujacy;
 
-            var obserwujacy = db.Obserwujacy.Where(o => o.ObserwowanyId == zalogowanyId);
+            if (czyKogosProfil == 0 || czyKogosProfil == 2)
+            {
+				obserwujacy = db.Obserwujacy.Where(o => o.ObserwowanyId == kogosId);
+			}
+            else
+            {
+				obserwujacy = db.Obserwujacy.Where(o => o.ObserwowanyId == zalogowanyId);
+			}
 
             foreach (Obserwujacy o in obserwujacy)
             {
                 listaUzytkownikow.Add(db.Uzytkownicy.Where(u => u.UzytkownikId == o.ObserwatorId).FirstOrDefault());
             }
-
             Page();
         }
 
-        public IActionResult OnPostAnulujBtn()
+        public IActionResult OnPostAnulujBtn(int czy, int id)
         {
-            return RedirectToPage("/ProfilPrywatny/Index");
+            if (czy == 1)
+            {
+                return RedirectToPage("/ProfilPrywatny/Index");
+            }
+            else
+            {
+                HttpContext.Session.SetInt32("PowrotneID", id);
+                return RedirectToPage("/ProfilPubliczny/Index");
+            }
         }
     }
 }
